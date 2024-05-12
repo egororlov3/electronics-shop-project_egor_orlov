9909cc6db901598ed8e24d48abf086463d157a9f
erop <erop.orl@gmail.com>
2024-05-12 16:24:48 +0300

    tests and new defs

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,18 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+@pytest.fixture
+def phone():
+    return Item("телефон", 100000, 2)
+
+
+def test_item_init(phone):
+    assert phone.calculate_total_price() == 200000
+
+def test_discount(phone):
+    Item.pay_rate = 1.5
+    assert phone.apply_discount() == 150000
+
+
+
+
